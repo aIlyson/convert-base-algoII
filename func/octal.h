9ff8a672c73f6e0284662d59d685ecd23bde0d25
Vinny00101 <vini123gtbr@gmail.com>
2025-04-01 17:00:57 -0300

    Folder func

---
+++ func/octal.h
@@ -0,0 +1,12 @@
+#ifndef OCTAL_CONVERTER
+#define OCTAL_CONVERTER
+
+// converte de Decimal para Octal
+int DecimalOctal(int);
+// converte de Octal para Decimal
+int OctalDecimal(int);
+
+int OctalBinario(int);
+// 7 = 111 6 = 110 0 = 000, 1 = 001, 2 = 010
+
+#endif
